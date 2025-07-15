f5735ae5d41fc64ec657d61c35011b713ae2f3ce
Victor Zhang <victorzw895@gmail.com>
2025-07-15 13:42:14 +1000

    feat: update config

--- next.config.js
+++ next.config.js
@@ -1,24 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'media.kitsu.io',
-      },
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-      }
-    ]
-  },
-  compiler: {
-    styledComponents: {
-      displayName: true,
-      ssr: true
-    },
-  }
+	reactStrictMode: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'media.kitsu.io',
+			},
+			{
+				protocol: 'https',
+				hostname: 'media.kitsu.app',
+			},
+			{
+				protocol: 'https',
+				hostname: 'placehold.co',
+			},
+		],
+	},
+	compiler: {
+		styledComponents: {
+			displayName: true,
+			ssr: true,
+		},
+	},
 }
 
 module.exports = nextConfig
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,43 +1,43 @@
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode } from 'react'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import ReactContextProvider from '@/providers/ReactContextProvider';
-import XStateProvider from '@/providers/XStateProvider';
-import Layout from '@/layout/Layout';
+import ReactContextProvider from '@/providers/ReactContextProvider'
+import XStateProvider from '@/providers/XStateProvider'
+import Layout from '@/layout/Layout'
 
-const useXState = process.env.NEXT_PUBLIC_XSTATE;
-const useZustand = process.env.NEXT_PUBLIC_ZUSTAND;
+const useXState = process.env.NEXT_PUBLIC_XSTATE
+const useZustand = process.env.NEXT_PUBLIC_ZUSTAND
 
 // console.info(`Preview State Management Provider: ${useXState ? 'XState Machine' : 'React Context'}`)
 
 interface ProvidersProps extends Partial<AppProps> {
-  children: ReactNode,
+	children: ReactNode
 }
 
-const Providers: FC<ProvidersProps> = ({children, pageProps}) => {
+const Providers: FC<ProvidersProps> = ({ children, pageProps }) => {
+	const noProviders = <>{children}</>
 
-  const noProviders = <>{children}</>
-
-  return (
-    <>
-      {
-        useZustand ? noProviders
-          :
-        useXState ? <XStateProvider pageProps={pageProps} />
-          :
-        <ReactContextProvider pageProps={pageProps} />
-      }
-    </>
-  )
+	return (
+		<>
+			{useZustand ? (
+				noProviders
+			) : useXState ? (
+				<XStateProvider pageProps={pageProps}>{children}</XStateProvider>
+			) : (
+				<ReactContextProvider pageProps={pageProps}>
+					{children}
+				</ReactContextProvider>
+			)}
+		</>
+	)
 }
 
 export default function App({ Component, pageProps }: AppProps) {
-
-  return (
-    <Layout>
-      <Providers pageProps={pageProps}>
-        <Component {...pageProps} />
-      </Providers>
-    </Layout>
-  )
+	return (
+		<Layout>
+			<Providers pageProps={pageProps}>
+				<Component {...pageProps} />
+			</Providers>
+		</Layout>
+	)
 }
